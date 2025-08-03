28a74bfbdffe4f0e788df0505c0a1a8f708ca7ec
TamaraHeater <tamaraheater@gmail.com>
2025-08-03 02:35:07 -0500

    errors of pakagej file

--- webpack.config.js
+++ webpack.config.js
@@ -54,7 +54,7 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: "./src/index.html",
-      favicon: ".src/images/favicon.ico",
+      favicon: "./src/images/favicon.ico",
     }),
     new CleanWebpackPlugin(),
     new MiniCssExtractPlugin(),
