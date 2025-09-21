b810bc21128b9d826857052eab607c910b36435c
TamaraHeater <tamaraheater@gmail.com>
2025-09-20 22:01:00 -0500

    Error Correct all MODALS drove back to STRICT lesson plan

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,6 @@
-// index.js
 import "../pages/index.css";
 import Api from "../utils/Api.js";
-import {
-  setButtonText,
-  renderLoading,
-  handleSubmit,
-} from "../utils/helpers.js";
+import { setButtonText, handleSubmit } from "../utils/helpers.js";
 import {
   enableValidation,
   validationConfig,
@@ -78,6 +73,11 @@ const modalPreviewImageEl = previewModal.querySelector(".modal__preview-image");
 const modalPreviewCaptionEl = previewModal.querySelector(
   ".modal__preview-caption"
 );
+//Delete Mode
+const deleteModal = document.querySelector("#delete-modal");
+const deleteFormElement = deleteModal.querySelector(".modal__form");
+const cancelDeleteButton = deleteModal.querySelector(".modal__cancel-button");
+const cardDeleteButton = deleteModal.querySelector(".modal__submit-button");
 
 api
   .getAppInfo()
@@ -123,6 +123,7 @@ function getCardElement(data) {
   const cardNameElement = cardElement.querySelector(".card__title");
   const cardImageElement = cardElement.querySelector(".card__image");
   const cardLikeButtonEl = cardElement.querySelector(".card__like-button");
+  const cardDeleteButton = cardElement.querySelector(".card__delete-button");
 
   cardNameElement.textContent = data.name;
   cardImageElement.src = data.link;
@@ -141,6 +142,12 @@ function getCardElement(data) {
     modalPreviewImageEl.alt = data.name;
     modalPreviewCaptionEl.textContent = data.name;
     openModal(previewModal);
+  });
+
+  cardDeleteButton.addEventListener("click", () => {
+    selectedCard = cardElement;
+    selectedCardID = data._id;
+    openModal(deleteModal);
   });
 
   return cardElement;
@@ -229,132 +236,61 @@ modals.forEach((modal) => {
   });
 });
 
-// Form Functions
-function handleNewPostFormSubmit(event) {
-  console.log("handleNewPostFormSubmit called");
-  event.preventDefault();
-  const form = event.target;
-  const imageInput = form.querySelector("#card-image-input").value;
-  const captionInput = form.querySelector("#card-caption-input").value;
-
-  renderLoading(true, form);
-
-  if (!imageInput) {
-    renderLoading(false, form, "Please enter an image URL");
-    console.log("Empty image URL");
-    return;
+//Form Handlers
+// Profile Form Handler
+editFormElement.addEventListener("submit", function (evt) {
+  function makeRequest() {
+    return api
+      .editUserInfo({
+        name: editModalNameInput.value,
+        about: editModalDescriptionInput.value,
+      })
+      .then((userData) => {
+        profileName.textContent = userData.name;
+        profileDescription.textContent = userData.about;
+        closeModal(editModal);
+        return userData;
+      });
   }
-  if (!captionInput) {
-    renderLoading(false, form, "Please enter a caption");
-    console.log("Empty caption");
-    return;
+  handleSubmit(makeRequest, evt, "Saving...");
+});
+
+newPostFormElement.addEventListener("submit", function (evt) {
+  function makeRequest() {
+    return api
+      .addCard({
+        name: newPostCaptionInput.value,
+        link: newPostImageInput.value,
+      })
+      .then((newPost) => {
+        const cardElement = getCardElement(newPost);
+        cardsList.prepend(cardElement);
+        closeModal(newPostModal);
+        return newPost;
+      });
   }
+  handleSubmit(makeRequest, evt, "Saving...");
+});
 
-  const request = function () {
-    return api.addCard({ name: captionInput, link: imageInput });
-  };
-
-  handleSubmit(request, event, "Saving...")
-    .then(function () {
-      closeModal(document.querySelector("#new-post-modal"));
-      renderCards();
-    })
-    .catch(function (error) {
-      console.log("Post error:", error);
-      renderLoading(false, form, "Cannot save post. Try again.");
+avatarFormElement.addEventListener("submit", function (evt) {
+  function makeRequest() {
+    return api.updateAvatar({ avatar: avatarInput.value }).then((data) => {
+      profileAvatar.src = data.avatar;
+      closeModal(avatarModal);
+      return data;
     });
-}
-
-// Profile Form
-function handleProfileFormSubmit(event) {
-  console.log("handleProfileFormSubmit called");
-  event.preventDefault();
-  const form = event.target;
-  const submitButton = form.querySelector(".modal__submit-button");
-  const nameInput = form.querySelector("#profile-name-input");
-  const descriptionInput = form.querySelector("#profile-description-input");
-
-  if (!nameInput || !descriptionInput) {
-    console.log("Profile inputs not found");
-    renderLoading(false, form, "Form error. Check input IDs.");
-    return;
   }
+  handleSubmit(makeRequest, evt, "Saving...");
+});
 
-  renderLoading(true, form);
-
-  if (!nameInput.value || !descriptionInput.value) {
-    renderLoading(false, form, "Please fill out all fields");
-    console.log("Empty profile fields");
-    return;
-  }
-
-  setButtonText(submitButton, true, "Save", "Saving...");
-  const request = function () {
-    return api.editUserInfo({
-      name: nameInput.value,
-      about: descriptionInput.value,
+deleteFormElement.addEventListener("submit", function (evt) {
+  function makeRequest() {
+    return api.deleteCard(selectedCardID).then(() => {
+      selectedCard.remove();
+      closeModal(deleteModal);
     });
-  };
-
-  handleSubmit(request, event, "Saving...")
-    .then(function ({ data }) {
-      document.querySelector(".profile__name").textContent = data.name;
-      document.querySelector(".profile__description").textContent = data.about;
-      closeModal(document.querySelector("#edit-open-modal"));
-    })
-    .catch(function (error) {
-      console.log("Profile error:", error);
-      renderLoading(false, form, "Cannot save profile. Try again.");
-    });
-}
-
-// Avatar Form
-function handleAvatarFormSubmit(event) {
-  console.log("handleAvatarFormSubmit called");
-  event.preventDefault();
-  const form = event.target;
-  const submitButton = form.querySelector(".modal__submit-button");
-  const avatarInput = form.querySelector("#profile-avatar-input");
-
-  if (!avatarInput) {
-    console.log("Avatar input not found");
-    renderLoading(false, form, "Form error. Check input ID.");
-    return;
   }
-
-  renderLoading(true, form);
-
-  if (!avatarInput.value) {
-    renderLoading(false, form, "Please enter an avatar URL");
-    console.log("Empty avatar URL");
-    return;
-  }
-
-  setButtonText(submitButton, true, "Save", "Saving...");
-  const request = function () {
-    return api.updateAvatar({ avatar: avatarInput.value });
-  };
-
-  handleSubmit(request, event, "Saving...")
-    .then(function ({ data }) {
-      document.querySelector("#profile-avatar").src = data.avatar;
-      closeModal(document.querySelector("#avatar-open-modal"));
-    })
-    .catch(function (error) {
-      console.log("Avatar error:", error);
-      renderLoading(false, form, "Cannot save avatar. Try again.");
-    });
-}
-
-// Event Listeners
-document
-  .querySelector("#new-post-form-modal")
-  .addEventListener("submit", handleNewPostFormSubmit);
-document
-  .querySelector("#edit-open-modal")
-  .addEventListener("submit", handleProfileFormSubmit);
-document
-  .querySelector("#avatar-open-modal")
-  .addEventListener("submit", handleAvatarFormSubmit);
+  handleSubmit(makeRequest, evt, "Deleting...");
+});
 
 enableValidation(validationConfig);
--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -4,73 +4,28 @@ export function setButtonText(
   defaultText = "Save",
   loadingText = "Saving..."
 ) {
-  if (!button) {
-    console.log("No button for setButtonText");
-    return;
+  if (isLoading) {
+    button.textContent = loadingText;
+  } else {
+    button.textContent = defaultText;
   }
-  console.log("setButtonText:", {
-    isLoading,
-    button,
-    defaultText,
-    loadingText,
-  });
-  button.textContent = isLoading ? loadingText : defaultText;
-  button.disabled = isLoading;
-}
-
-export function renderLoading(isLoading, formElement, errorMessage = "") {
-  if (!formElement) {
-    console.log("No form for renderLoading");
-    return;
-  }
-  const errorSpans = formElement.querySelectorAll(".modal__error_visible");
-  console.log("renderLoading:", { isLoading, errorMessage });
-  errorSpans.forEach((span) => {
-    span.textContent = isLoading ? "" : errorMessage;
-  });
 }
 
 export function handleSubmit(request, evt, loadingText = "Saving...") {
   evt.preventDefault();
-  const submitButton =
-    evt.submitter || evt.target.querySelector(".modal__submit-button");
-  if (!submitButton) {
-    console.log("No submit button found");
-    return;
-  }
-  const initialText = submitButton.textContent || "Save";
-  console.log("handleSubmit calling setButtonText:", {
-    isLoading: true,
-    submitButton,
-    initialText,
-    loadingText,
-  });
+  const submitButton = evt.submitter;
+  const initialText = submitButton.textContent;
   setButtonText(submitButton, true, initialText, loadingText);
-  renderLoading(true, evt.target);
-
   return request()
-    .then(function (response) {
-      if (!response.ok) {
-        console.log("Server error: HTTP", response.status);
-        throw new Error();
-      }
-      return response.json();
-    })
-    .then(function (data) {
+    .then((data) => {
       evt.target.reset();
       return data;
     })
-    .catch(function (err) {
-      console.log("Request failed:", err);
-      renderLoading(false, evt.target, "Cannot save. Try again.");
+    .catch((err) => {
+      console.error("Submission failed:", err);
       throw err;
     })
-    .finally(function () {
-      console.log("handleSubmit calling setButtonText:", {
-        isLoading: false,
-        submitButton,
-        initialText,
-      });
-      setButtonText(submitButton, false, initialText);
+    .finally(() => {
+      setButtonText(submitButton, false, initialText, loadingText);
     });
 }
